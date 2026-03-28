7324da2d9864c485ab17df1770b7b075a58a897d
Yamuna Latchipatruni <yamunal868@gmail.com>
2026-03-28 21:17:01 +0530

    fix cors for deployment

--- backend/main.py
+++ backend/main.py
@@ -8,11 +8,7 @@ app = FastAPI()
 # Allow frontend (Next.js)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "http://192.168.56.1:3000"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
